ce97601d555477d545ef57ffeb6442db4ca109e3
Alencar da Costa <alencar.dacosta@moovin.com.br>
2022-02-26 17:34:40 -0300

    feat: add brightness value min max

--- effects/brightness.go
+++ effects/brightness.go
@@ -15,14 +15,16 @@ func (b *Brightness) Apply(img *photochopp.Image) (err error) {
 		return errors.New("effect: cannot apply brightness to a nil image")
 	}
 
+	value := int(math.Max(-255, math.Min(float64(b.Value), 255)))
+
 	width, height := img.Width(), img.Height()
 
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
 			pixel := img.Pixel(x, y)
-			pixel[0] = uint8(math.Max(0, math.Min(float64(int(pixel[0])+b.Value), 255)))
-			pixel[1] = uint8(math.Max(0, math.Min(float64(int(pixel[1])+b.Value), 255)))
-			pixel[2] = uint8(math.Max(0, math.Min(float64(int(pixel[2])+b.Value), 255)))
+			pixel[0] = uint8(math.Max(0, math.Min(float64(int(pixel[0])+value), 255)))
+			pixel[1] = uint8(math.Max(0, math.Min(float64(int(pixel[1])+value), 255)))
+			pixel[2] = uint8(math.Max(0, math.Min(float64(int(pixel[2])+value), 255)))
 		}
 	}
 

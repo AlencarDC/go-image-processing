1fb57319334b1ba2f2a8cc6d509dfb7cea0f76dc
Alencar da Costa <alencar.dacosta@moovin.com.br>
2022-02-26 17:07:07 -0300

    refactor: move app creation logic to a custom data structure

---
+++ gui/app.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
+
+type App struct {
+	instance fyne.App
+	windows  map[string]fyne.Window
+}
+
+func (app *App) Start() {
+	window, hasMainWindow := app.windows["main"]
+
+	if !hasMainWindow {
+		log.Println("app: main window is not defined")
+		return
+	}
+
+	window.ShowAndRun()
+	log.Println("app: started")
+}
+
+func (app *App) NewWindow(id, name string, width, height float32) fyne.Window {
+	window := app.instance.NewWindow(name)
+	window.Resize(fyne.NewSize(width, height))
+
+	app.windows[id] = window
+
+	return window
+}
+
+func NewApp(name string, width, height float32) (*App, error) {
+
+	application := new(App)
+	application.instance = app.New()
+	application.windows = make(map[string]fyne.Window)
+
+	window := application.NewWindow("main", name, width, height)
+	screen := NewMainScreen(*application, window)
+	window.SetContent(screen.Content())
+
+	return application, nil
+}

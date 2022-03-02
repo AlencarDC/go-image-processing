99ada012a119545d148372824e9a64bae53cbe9f
Alencar da Costa <alencar.dacosta@moovin.com.br>
2022-03-02 19:34:51 -0300

    chore: change version

--- gui/main.go
+++ gui/main.go
@@ -4,7 +4,7 @@ import "log"
 
 func main() {
 
-	app, err := NewApp("Photochopp v1.0", 1280, 720)
+	app, err := NewApp("Photochopp v2.0", 1280, 720)
 	if err != nil {
 		log.Println("app: could not create new app", err)
 	}
